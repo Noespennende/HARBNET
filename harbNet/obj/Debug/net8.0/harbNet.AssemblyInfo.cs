7570429e878479b0ef479e3516605b25a306b0b5
Andreasberg89 <andber9@gmail.com>
2024-01-18 17:42:55 +0100

    metadata
    metadata

--- harbNet/obj/Debug/net8.0/harbNet.AssemblyInfo.cs
+++ harbNet/obj/Debug/net8.0/harbNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("harbNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f260df6d57a52a142aa42fb582b689321f5aae8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("harbNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("harbNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
