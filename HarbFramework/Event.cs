1f3c9b98b48d39f46ffcd25d174d41b1a3e18488
M1kke95 <m1kke95@hotmail.com>
2024-02-12 19:48:08 +0100

    xml og bugfix
    bugfix i simulation og xml i event, simulation, Ievent, Isimulation

--- HarbFramework/Event.cs
+++ HarbFramework/Event.cs
@@ -8,11 +8,30 @@ namespace HarbFramework
 {
     public class Event : IEvent
     {
-        public Guid Subject {  get; internal set; }
+        /// <summary>
+        /// Gets an unique Id of the subject
+        /// </summary>
+        public Guid Subject { get; internal set; }
+        /// <summary>
+        /// gets an unique ID of the location of a subject
+        /// </summary>
         public Guid SubjectLocation { get; internal set; }
+        /// <summary>
+        /// gets the point in time of an event or action. 
+        /// </summary>
         public DateTime PointInTime { get; internal set; }
+        /// <summary>
+        /// gets The status of the subject
+        /// </summary>
         public Status Status { get; internal set; }
 
+        /// <summary>
+        /// Constructor for an Event
+        /// </summary>
+        /// <param name="subject">The subject of the event</param>
+        /// <param name="subjectLocation">The location of the subject</param>
+        /// <param name="pointInTime">The point in time for this event</param>
+        /// <param name="status">The status of the event, f.eks Undocking, Loading, Unloading</param>
         internal Event (Guid subject, Guid subjectLocation, DateTime pointInTime, Status status)
         {
             this.Subject = subject;
--- HarbFramework/ISimulation.cs
+++ HarbFramework/ISimulation.cs
@@ -1,10 +1,24 @@
 ﻿namespace HarbFramework
 {
     public interface ISimulation
-    {
+    {   
+        /// <summary>
+        /// A list of log history
+        /// </summary>
         public IList<Log> History { get; }
+
+        /// <summary>
+        /// Starting the simulation
+        /// </summary>
+        /// <returns>The simulation results</returns>
         public IList<Log> Run();
+        /// <summary>
+        /// Prints the ships history
+        /// </summary>
         public void PrintShipHistory();
+        /// <summary>
+        /// prints the containers history
+        /// </summary>
         public void PrintContainerHistory();
     }
 }
