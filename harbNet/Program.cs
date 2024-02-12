c0ae10dc909ecaac6e5e66772f4a5159785ae79d
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-02-12 21:03:48 +0100

    Start på eksempelprogram

--- harbNet/Program.cs
+++ harbNet/Program.cs
@@ -10,35 +10,38 @@ namespace harbNet
         {
 
 
-            DateTime startTime = new DateTime(2023, 2, 2, 8, 0, 0);
-            DateTime endTime = startTime + TimeSpan.FromDays(40);
+            DateTime startTime = new DateTime(2024, 3, 1, 8, 0, 0);
+            DateTime endTime = startTime + TimeSpan.FromDays(30);
             List<Ship> ships = new List<Ship>();
 
-            Ship testShip = new("SS MonkeyPaw", ShipSize.Large, startTime, false, 2, 68);
-            ships.Add(testShip);
+            //Ship shipHappens = new("Ship Happens", ShipSize.Large, startTime, false, 7, 50);
+            
+            Ship auroraBorealis = new("Aurora Borealis", ShipSize.Medium, startTime.AddDays(4), true, 3, 49);
+            //Ship skipOHoi = new("Ship O'Hoi", ShipSize.Small, startTime.AddHours(4), false, 1, 15);
+            //Ship ssSolitude = new("SS Solitude", ShipSize.Small, startTime, false, 14, 1);
+
+            //ships.Add(shipHappens);
+            //ships.Add(ssSolitude);
+            ships.Add(auroraBorealis);
+            //ships.Add(skipOHoi);
+
+            
+
+            Harbor kjuttaviga = new Harbor(ships, 4, 3, 2, 2, 1, 1, 100, 200, 150);
 
 
-            Ship oneTripShip = new("SS OneTrip", ShipSize.Small, startTime.AddDays(5), true, 1, 2);
-            ships.Add(oneTripShip);
-
-
-            Harbor harbor = new Harbor(ships, 10, 10, 10, 300, 300, 300, 300, 300, 300);
-
-
-            Simulation simulation = new Simulation(harbor, startTime, endTime);
-
+            Simulation simulation = new Simulation(kjuttaviga, startTime, endTime);
 
             IList<Log> historyList = simulation.Run();
 
-            oneTripShip.PrintHistory();
+            Console.WriteLine(auroraBorealis.ContainersOnBoard.Count);
+
+            
 
 
+            //Console.WriteLine("-------------------------");
 
-
-
-            Console.WriteLine("-------------------------");
-
-            simulation.PrintContainerHistory();
+            //simulation.PrintContainerHistory();
 
 
 
