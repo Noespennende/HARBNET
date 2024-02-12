f9239970a1cf3b60ecb3cf9c82b457503b543ff2
Andreasberg89 <andber9@gmail.com>
2024-02-12 21:52:49 +0100

    Litt ekstra kommentarer til eksempelprosjektet

--- harbNet/Program.cs
+++ harbNet/Program.cs
@@ -8,7 +8,11 @@ namespace harbNet
     {
         static void Main(string[] args)
         {
+            //EXAMPLE PROJECT
 
+            /*This project sets up and runs a simulation using the public API. Several methods for printing information about the simulation
+             is used simultaniously here (ShipObject.PrintHistory(), SimulationObject.PrintContainerHistory() and SimulationObject.PrintShipHistory()) 
+            so the consol log will be cluttered with information. Normally only one of these methods would be enough to get information about the simulation. */
 
             // Setting a time for the start time of the simulation,  which will be used in the creation of Simulation object
             DateTime startTime = new DateTime(2024, 3, 1, 8, 0, 0);
@@ -35,15 +39,22 @@ namespace harbNet
             // Creating the simulation object, of which the simulation will run from
             Simulation simulation = new Simulation(kjuttaviga, startTime, endTime);
 
-            // Running/starting the Simulation. Run() outputs a list of logs that is created during the simulation. This is 
+            /* Running/starting the Simulation. Run() outputs a list of logs that is created during the simulation. It's worth noting that simulation.Run() will also print
+            updates on all ships during the simulation*/
             simulation.Run();
 
 
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("\n-------------------------\n");
 
+            //Prints the history for a single ship
             ssSolitude.PrintHistory();
 
+            Console.WriteLine("\n-------------------------\n");
+            //Prints the history for all containers in the simulation
             simulation.PrintContainerHistory();
+
+            Console.WriteLine("\n-------------------------\n");
+            //Prints the history for all ships in the simulation
             simulation.PrintShipHistory();
 
            
