af84aa72b3886d5e8cfadf603781a8b58d3f2b25
M1kke95 <m1kke95@hotmail.com>
2024-03-02 21:29:16 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/Noespennende/HARBNET""
    This reverts commit bd7e326815815b262e533cd36fad03a60b81cecc.

---
+++ HarbFramework/TestException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarbFramework
+{
+    public class TestException : Exception
+    {
+
+        public TestException() 
+        { 
+        }
+
+        public TestException(string message) 
+        { 
+        }
+
+        public TestException(string message, Exception inner) 
+        { 
+        }
+
+
+        protected TestException(SerializationInfo info, StreamingContext context) 
+        { 
+        }
+        
+    }
+}
